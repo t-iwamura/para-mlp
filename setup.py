6d57f5cae03720b799b93b39a3cdeb140f4489d5
Taiki Iwamura <takki.0206@gmail.com>
2022-03-15 12:27:26 +0000

    Revise setup.*

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='para-mlp',
     version='0.0.1',
     author='Taiki Iwamura',
@@ -17,7 +17,6 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
-    python_requires='>=3.6',
+    packages=['para-mlp'],
+    python_requires='>=3.7',
 )
