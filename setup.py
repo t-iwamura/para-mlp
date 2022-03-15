d90fa53456f3fdd6cb115b5764d6a4bf4ad4b1ce
Taiki Iwamura <takki.0206@gmail.com>
2022-03-15 01:11:34 +0000

    Revise setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
+
+setuptools.setup(
+    name='para-mlp-iwamura',
+    version='0.0.1',
+    author='Taiki Iwamura',
+    author_email='iwamura.taiki.43e@st.kyoto-u.ac.jp',
+    description='paramagnetic machine learning potential package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/iwamura-lab/para-mlp',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.6',
+)
