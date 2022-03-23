0e92b328860056d01c97b948178fb7d5fc1d0380
Taiki Iwamura <takki.0206@gmail.com>
2022-03-23 08:25:51 +0000

    Add train()

--- para_mlp/train.py
+++ para_mlp/train.py
@@ -1,3 +1,5 @@
+import copy
+
 from para_mlp.data_structure import ModelParams
 from para_mlp.preprocess import make_model_params
 
@@ -7,21 +9,24 @@ def train():
     model_params = {}
     model_params["use_force"] = False
     model_params["use_stress"] = False
+    model_params["composite_num"] = 1
     model_params["polynomial_model"] = 1
     model_params["polynomial_max_order"] = 2
-    model_params["radial_func"] = "gaussian"
     model_params["feature_type"] = "gtinv"
-    model_params["composite_num"] = 1
     model_params["cutoff_radius"] = 6.0
+    model_params["radial_func"] = "gaussian"
     model_params["atomic_energy"] = -3.37689
+
     hyper_params = {}
     hyper_params["gaussian_params1"] = (1.0, 1.0, 1)
     hyper_params["gaussian_params2"] = (1.0, 5.0, 10)
     hyper_params["gtinv_order"] = 2
-    hyper_params["gtinv_lmax"] = 3
-    hyper_params["gtinv_sym"] = 0
-    model_params["lmax"] = hyper_params["gtinv_lmax"]
+    hyper_params["gtinv_lmax"] = [3]
+    hyper_params["gtinv_sym"] = [False]
+    model_params["lmax"] = copy.copy(hyper_params["gtinv_lmax"])
 
     model_params = ModelParams.from_dict(make_model_params(hyper_params, model_params))
 
     # TODO: kf = KFold(n_splits=10)
+
+    return model_params
