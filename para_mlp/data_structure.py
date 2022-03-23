edf40def8acdf3371c42cc282b288574e576b531
Taiki Iwamura <takki.0206@gmail.com>
2022-03-23 08:24:21 +0000

    Make template of data_structure.py

--- para_mlp/data_structure.py
+++ para_mlp/data_structure.py
@@ -1,8 +1,26 @@
 from dataclasses import dataclass
+from typing import Any
+
+from dataclasses_json import dataclass_json
 
 
+@dataclass_json
 @dataclass
 class ModelParams:
+    # what data to use
     use_force: bool = False
     use_stress: bool = False
-    composite_num: int = 2
+    # functional form
+    composite_num: int = 1
+    polynomial_model: int = 1
+    polynomial_max_order: int = 2
+    # feature settings
+    feature_type: str = "gtinv"
+    cutoff_radius: float = 6.0
+    lmax: Any = None
+    lm_seq: Any = None
+    l_comb: Any = None
+    lm_coeffs: Any = None
+    radial_func: str = "gaussian"
+    radial_params: Any = None
+    atomic_energy: float = None
