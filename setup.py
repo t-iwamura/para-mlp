e28c314c86678d86704090421a7f95463a7bfc9d
Taiki Iwamura <takki.0206@gmail.com>
2022-03-25 02:37:00 +0000

    Bumped version number to 1.1.0

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='para_mlp',
-    version='0.0.1',
-    author='Taiki Iwamura',
-    author_email='takki.0206@gmail.com',
-    description='paramagnetic machine learning potential package',
+    name="para_mlp",
+    version="1.1.0",
+    author="Taiki Iwamura",
+    author_email="takki.0206@gmail.com",
+    description="paramagnetic machine learning potential package",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/iwamura-lab/para-mlp',
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwamura-lab/para-mlp",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    packages=['para_mlp'],
-    python_requires='>=3.7',
+    packages=["para_mlp"],
+    python_requires=">=3.7",
 )
