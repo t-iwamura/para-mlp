e3c6e8fafe9a992c12a578e9915ddc846b066b1b
Taiki Iwamura <takki.0206@gmail.com>
2022-03-28 10:34:41 +0000

    Make para_mlp/data_structure.py more user friendly

--- para_mlp/data_structure.py
+++ para_mlp/data_structure.py
@@ -1,7 +1,7 @@
 import copy
 from dataclasses import dataclass
 from itertools import product
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 import numpy as np
 from dataclasses_json import dataclass_json
@@ -37,31 +37,41 @@ class ModelParams:
     # what data to use
     use_force: bool = False
     use_stress: bool = False
+    atomic_energy: float = -3.37689
     # functional form
     composite_num: int = 1
     polynomial_model: int = 1
     polynomial_max_order: int = 2
     # feature settings
+    # API params
     feature_type: str = "gtinv"
     cutoff_radius: float = 6.0
+    radial_func: str = "gaussian"
+    gaussian_params1: Tuple[float, float, int] = (1.0, 1.0, 1)
+    gaussian_params2: Tuple[float, float, int] = (1.0, 5.0, 10)
+    gtinv_order: float = 2
+    gtinv_lmax: Tuple[float] = (3,)
+    gtinv_sym: Tuple[bool] = (False,)
+    # naive params
     lmax: Any = None
     lm_seq: Any = None
     l_comb: Any = None
     lm_coeffs: Any = None
-    radial_func: str = "gaussian"
     radial_params: Any = None
-    atomic_energy: float = -3.37689
+    # misc
+    alpha: float = None
 
     def make_feature_params(self) -> None:
-        hyper_params: Dict[str, Any] = {}
-        hyper_params["gaussian_params1"] = (1.0, 1.0, 1)
-        hyper_params["gaussian_params2"] = (1.0, 5.0, 10)
-        hyper_params["gtinv_order"] = 2
-        hyper_params["gtinv_lmax"] = [3]
-        self.lmax = copy.deepcopy(hyper_params["gtinv_lmax"])[0]
-        hyper_params["gtinv_sym"] = [False]
-
+        hyper_params: Dict[str, Any] = {
+            "gaussian_params1": list(self.gaussian_params1),
+            "gaussian_params2": list(self.gaussian_params2),
+            "gtinv_order": self.gtinv_order,
+            "gtinv_lmax": [i for i in self.gtinv_lmax],
+            "gtinv_sym": [i for i in self.gtinv_sym],
+        }
         new_model_params = make_model_params(hyper_params)
+
+        self.lmax = copy.deepcopy(hyper_params["gtinv_lmax"])[0]
 
         self.lm_seq = new_model_params["lm_seq"]
         self.l_comb = new_model_params["l_comb"]
