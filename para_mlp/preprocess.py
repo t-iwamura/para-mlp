3f2a1f3c553d30e17c0672f77a6541cc6068878a
Taiki Iwamura <takki.0206@gmail.com>
2022-03-29 18:29:48 +0900

    Add function to create dataset for jsons

--- para_mlp/preprocess.py
+++ para_mlp/preprocess.py
@@ -1,9 +1,11 @@
 import json
 import sys
+from itertools import chain
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Any, Dict, Tuple
 
+import numpy as np
 from mlp_build_tools.mlpgen.myIO import ReadVaspruns
 from pymatgen.core.structure import Structure
 from sklearn.model_selection import train_test_split
@@ -12,6 +14,65 @@ mlp_build_tools_path = (
     Path.home() / "mlp-Fe" / "mlptools" / "mlp_build_tools" / "cpp" / "lib"
 )
 sys.path.append(mlp_build_tools_path.as_posix())
+
+
+def create_dataset(
+    data_dir: str, targets_json: str, use_force: bool = False
+) -> Dict[str, Any]:
+    data_dir_path = Path(data_dir)
+    if data_dir_path.exists():
+        inputs_dir_path = data_dir_path / "inputs" / "data"
+    else:
+        raise FileNotFoundError(f"data_dir does not exist: {data_dir}")
+
+    targets_json_path = Path(targets_json)
+    if targets_json_path.exists():
+        with targets_json_path.open("r") as f:
+            structure_ids = json.load(f)
+    else:
+        raise FileNotFoundError(f"targets_json_path does not exist: {targets_json}")
+
+    structures = []
+    energies = []
+    forces = []
+    for sid in structure_ids:
+        # Load vasp_outputs.json
+        vasp_outputs_json_path = inputs_dir_path / sid / "vasp_outputs.json"
+        with vasp_outputs_json_path.open("r") as f:
+            vasp_outputs = json.load(f)
+
+        energies.append(vasp_outputs["energy"])
+
+        if use_force:
+            for force_component in chain.from_iterable(vasp_outputs["force"]):
+                forces.append(force_component)
+
+        # Load structure.json
+        structure_json_path = inputs_dir_path / sid / "structure.json"
+        with structure_json_path.open("r") as f:
+            structure_dict = json.load(f)
+        structure = Structure.from_dict(structure_dict)
+        structures.append(structure)
+
+    dataset = {"energy": np.array(energies), "structures": structures}
+
+    if use_force:
+        dataset["force"] = np.array(forces)
+
+    return dataset
+
+
+def split_dataset(
+    dataset: Dict[str, Any] = None, test_size: float = 0.1, shuffle: bool = True
+) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    structure_train, structure_test, y_train, y_test = train_test_split(
+        dataset["structures"], dataset["energy"], test_size=test_size, shuffle=shuffle
+    )
+
+    kfold_dataset = {"structures": structure_train, "energy": y_train}
+    test_dataset = {"structures": structure_test, "energy": y_test}
+
+    return kfold_dataset, test_dataset
 
 
 def make_vasprun_tempfile(data_dir: str, targets_json: str) -> str:
@@ -41,7 +102,7 @@ def make_vasprun_tempfile(data_dir: str, targets_json: str) -> str:
     return temp_object.name
 
 
-def create_dataset(data_dir: str, targets_json: str) -> Dict[str, Any]:
+def create_dataset_by_seko_method(data_dir: str, targets_json: str) -> Dict[str, Any]:
     data_dir_path = Path(data_dir)
     if not data_dir_path.exists():
         raise FileNotFoundError(f"data_dir does not exist: {data_dir}")
@@ -70,16 +131,3 @@ def create_dataset(data_dir: str, targets_json: str) -> Dict[str, Any]:
     dataset = {"energy": energy, "structures": structures}
 
     return dataset
-
-
-def split_dataset(
-    dataset: Dict[str, Any] = None, test_size: float = 0.1, shuffle: bool = True
-) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    structure_train, structure_test, y_train, y_test = train_test_split(
-        dataset["structures"], dataset["energy"], test_size=test_size, shuffle=shuffle
-    )
-
-    kfold_dataset = {"structures": structure_train, "energy": y_train}
-    test_dataset = {"structures": structure_test, "energy": y_test}
-
-    return kfold_dataset, test_dataset
