f8512294015aa5379c62683e7c088468fb06ac74
Taiki Iwamura <takki.0206@gmail.com>
2022-03-30 18:41:57 +0900

    Minor update in tests/test_pred.py

--- tests/test_pred.py
+++ tests/test_pred.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def test_train_and_eval(train_output, loaded_model_object, divided_dataset):
+def test_load_model(train_output, loaded_model_object, divided_dataset):
     obtained_model, obtained_model_params = train_output
 
     assert obtained_model_params == loaded_model_object["model_params"]
