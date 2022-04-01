821836f58723fc96cf5bf2ad4c84d2b67e91cb49
Taiki Iwamura <takki.0206@gmail.com>
2022-04-01 18:00:20 +0900

    Add docstring in utils.py

--- para_mlp/utils.py
+++ para_mlp/utils.py
@@ -3,4 +3,13 @@ from numpy.typing import NDArray
 
 
 def rmse(y_predict: NDArray, y_target: NDArray) -> float:
+    """Calculate RMSE of objective variable
+
+    Args:
+        y_predict (NDArray): predicted objective variable
+        y_target (NDArray): target objected variable
+
+    Returns:
+        float: RMSE
+    """
     return np.sqrt(np.mean(np.square(y_predict - y_target)))
