83823f49d213ab286d098539da7e582555bed24a
Taiki Iwamura <takki.0206@gmail.com>
2022-05-22 11:05:18 +0900

    Add pytest for two_specie in test_model.py::test_make_content_of_lammps_file()

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,27 +3,31 @@ import numpy as np
 from para_mlp.model import make_content_of_lammps_file
 
 
-def test_make_content_of_lammps_file(trained_model_multiconfig, seko_lammps_file_lines):
-    trained_model = trained_model_multiconfig["one_specie"]
-    generated_lines = make_content_of_lammps_file(trained_model).split("\n")
+def test_make_content_of_lammps_file(
+    trained_model_multiconfig, seko_lammps_file_lines_multiconfig
+):
+    for config_key in trained_model_multiconfig.keys():
+        trained_model = trained_model_multiconfig[config_key]
+        generated_lines = make_content_of_lammps_file(trained_model).split("\n")
+        seko_lammps_file_lines = seko_lammps_file_lines_multiconfig[config_key]
 
-    # Comparison of top blocks
-    assert generated_lines[:11] == seko_lammps_file_lines[:11]
-    # Comparison of bottom blocks
-    assert generated_lines[-14:] == seko_lammps_file_lines[-14:]
+        # Comparison of top blocks
+        assert generated_lines[:11] == seko_lammps_file_lines[:11]
+        # Comparison of bottom blocks
+        assert generated_lines[-14:] == seko_lammps_file_lines[-14:]
 
-    generated_middle_lines = [
-        [float(item) for item in line.split()]
-        for line in generated_lines[11:-14]
-        if "#" not in line
-    ]
-    seko_middle_lines = [
-        [float(item) for item in line.split()]
-        for line in seko_lammps_file_lines[11:-14]
-        if "#" not in line
-    ]
+        generated_middle_lines = [
+            [float(item) for item in line.split()]
+            for line in generated_lines[11:-14]
+            if "#" not in line
+        ]
+        seko_middle_lines = [
+            [float(item) for item in line.split()]
+            for line in seko_lammps_file_lines[11:-14]
+            if "#" not in line
+        ]
 
-    assert generated_middle_lines == seko_middle_lines
+        assert generated_middle_lines == seko_middle_lines
 
 
 def test_load_model(
