eff90fef91e912cf86cce2e408e04947ff1ecd36
Taiki Iwamura <takki.0206@gmail.com>
2023-04-09 22:21:52 +0900

    Move test_make_high_energy_yids() to tests/test_preprocess.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,7 +57,12 @@ def test_config():
 @pytest.fixture()
 def high_energy_sids() -> List[int]:
     high_energy_structures_path = (
-        PROCESSING_DIR_PATH / "sample_weight" / "high_energy_structures1"
+        INPUTS_DIR_PATH
+        / "sample_weight"
+        / "inputs"
+        / "sqs"
+        / "processing"
+        / "high_energy_structures1"
     )
     with high_energy_structures_path.open("r") as f:
         sids = [int(sid) - 1 for sid in f]
@@ -66,7 +71,15 @@ def high_energy_sids() -> List[int]:
 
 @pytest.fixture()
 def yids_for_kfold_high_energy():
-    yids_for_kfold_path = PROCESSING_DIR_PATH / "sample_weight" / "yid_kfold.json"
+    yids_for_kfold_path = (
+        INPUTS_DIR_PATH
+        / "sample_weight"
+        / "inputs"
+        / "sqs"
+        / "processing"
+        / "use_force_too"
+        / "yid_kfold.json"
+    )
     with yids_for_kfold_path.open("r") as f:
         yids_for_kfold = json.load(f)
 
@@ -75,8 +88,10 @@ def yids_for_kfold_high_energy():
 
 @pytest.fixture()
 def expected_high_energy_yids() -> Dict[str, NDArray]:
-    sample_weight_dir_path = PROCESSING_DIR_PATH / "sample_weight"
-    high_energy_yids_json_path = sample_weight_dir_path / "high_energy_yids.json"
+    processing_dir_path = (
+        INPUTS_DIR_PATH / "sample_weight" / "inputs" / "sqs" / "processing"
+    )
+    high_energy_yids_json_path = processing_dir_path / "high_energy_yids.json"
     with high_energy_yids_json_path.open("r") as f:
         high_energy_yids = json.load(f)
 
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -3,7 +3,7 @@ import pytest
 
 from para_mlp.data_structure import ModelParams
 from para_mlp.featurize import RotationInvariant, SpinFeaturizer
-from para_mlp.preprocess import make_force_id, merge_sub_dataset
+from para_mlp.preprocess import make_force_id, make_high_energy_yids, merge_sub_dataset
 
 
 @pytest.mark.parametrize(
@@ -12,6 +12,25 @@ from para_mlp.preprocess import make_force_id, merge_sub_dataset
 )
 def test_make_force_id(sid, atom_id, force_comp, expected):
     assert make_force_id(sid, atom_id, force_comp, n_atom=32) == expected
+
+
+def test_make_high_energy_yids(
+    high_energy_sids,
+    yids_for_kfold_high_energy,
+    expected_high_energy_yids,
+):
+    high_energy_yids = make_high_energy_yids(
+        high_energy_sids=high_energy_sids,
+        n_structure=100,
+        force_id_unit=12,
+        yids_for_kfold=yids_for_kfold_high_energy,
+        use_force=True,
+    )
+    np.testing.assert_equal(high_energy_yids, expected_high_energy_yids)
+
+
+def test_make_high_energy_struct_dicts():
+    pass
 
 
 def test_load_vasp_outputs(dataset_multiconfig, seko_vasprun_outputs_multiconfig):
