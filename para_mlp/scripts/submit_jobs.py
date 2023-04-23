3c3e9aa7b3d20c3bd9e33892e56d133166eb2356
Taiki Iwamura <takki.0206@gmail.com>
2023-04-23 21:04:24 +0900

    Fix the job name in para_mlp/scripts/submit_jobs.py

--- para_mlp/scripts/submit_jobs.py
+++ para_mlp/scripts/submit_jobs.py
@@ -38,13 +38,14 @@ def make_job_script(name: str) -> str:
 @click.option(
     "--max_id", required=True, type=int, help="maximum id of searching directories"
 )
+@click.option("--pool_dir_id", required=True, help="model pool directory id")
 @click.option(
     "-p", "--partition", default="vega-d", show_default=True, help="partition name"
 )
 @click.option(
     "--id_digits", type=int, default=3, show_default=True, help="digits filled by zero"
 )
-def main(min_id, max_id, partition, id_digits):
+def main(min_id, max_id, pool_dir_id, partition, id_digits):
     """Usefull package to submit multiple para-mlp jobs"""
     root_dir_path = Path.cwd()
     inputs_dir_path_list = [
@@ -58,7 +59,7 @@ def main(min_id, max_id, partition, id_digits):
 
         job_script_path = dir_path / "job.sh"
         with job_script_path.open("w") as f:
-            name = "".join(["para", dir_path.stem])
+            name = "-".join([pool_dir_id, dir_path.name])
             job_script_content = make_job_script(name)
             f.write(job_script_content)
 
