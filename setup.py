5391227d2edaf274e5a5a6223417ca44da0b1282
Taiki Iwamura <takki.0206@gmail.com>
2023-05-31 13:58:38 +0900

    Bumped version number to 1.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="para_mlp",
-    version="1.4.0",
+    version="1.4.1",
     author="Taiki Iwamura",
     author_email="takki.0206@gmail.com",
     description="paramagnetic machine learning potential package",
